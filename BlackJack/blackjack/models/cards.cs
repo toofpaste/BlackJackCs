86fbe8d12398364c17b04b12b47a377b5dce4179
Joe Barnes and Michael Larragueta <michael@>
2019-04-22 16:34:59 -0700

    Add blackJack

---
+++ BlackJack/blackjack/models/cards.cs
@@ -0,0 +1,58 @@
+﻿using System;
+namespace BlackJack
+{
+    public class Cards
+    {
+        private int Card;
+        private int Deal; 
+
+        public Cards(int card1, int card2)
+        {
+            Card = card1;
+            Deal = card2;
+        }
+        public int hitCard(int card1)
+        {
+            return Card += card1;
+        }
+        public int stayCard(int deal1)
+        {
+            return Deal += deal1;
+        }
+
+        public int getCard()
+        {
+            return Card;
+        }
+
+        public int getDeal()
+        {
+            return Deal;
+        }
+
+        public int checkWin(int deal, int card)
+        {
+            if (card > 21)
+            {
+                return 1;
+            }
+            else if (card < deal)
+            {
+                return 1;
+            }
+            else if (card > deal)
+            {
+                return 2;
+            }
+            else if (card == deal)
+            {
+                return 0;
+            }
+            else if (deal > 21)
+            {
+                return 2;
+            }
+            else return 0;
+        }
+    }
+}
---
+++ blackjack/BlackJack/BlackJack.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace BlackJack
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            Random rnd = new Random();
+
+            int card1 = rnd.Next(2, 11);
+            int card2 = rnd.Next(2, 11);
+
+            int deal = rnd.Next(2, 11);
+
+
+            Cards gameCard = new Cards((card1 + card2), deal);
+            Console.WriteLine("Dealer: " + gameCard.getDeal());
+            Console.WriteLine("User: " + gameCard.getCard());
+            int i = 0;
+
+
+            while (i == 0)
+            {
+                Console.WriteLine("HIT/STAY/QUIT");
+                bool ans = Console.ReadLine().ToLower() == "hit";
+
+                if (ans)
+                {
+                    int card3 = rnd.Next(2, 11);
+                    gameCard.hitCard(card3);
+                    Console.WriteLine("User: " + gameCard.getCard());
+                }
+                else
+                {
+                    while (gameCard.getDeal() < 17)
+                    {
+                        int deal1 = rnd.Next(2, 11);
+                        gameCard.stayCard(deal1);
+                    };
+                    i = 1;
+                    break;
+
+                
+                }
+            }
+            int win = gameCard.checkWin(gameCard.getDeal(), gameCard.getCard());
+            Console.WriteLine("Dealer: " + gameCard.getDeal());
+            Console.WriteLine("User: " + gameCard.getCard());
+            if (win == 2)
+            {
+                Console.WriteLine("User Wins");
+            }
+            else if (win == 1)
+            {
+                Console.WriteLine("Dealer Wins");
+            }
+            else if (win == 0)
+            {
+                Console.WriteLine("TIE TIE");
+            }
+        }
+    }
+}
+
